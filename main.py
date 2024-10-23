917da59154f57b750daffc56ea1e5bb1471afb8e
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-10-23 20:40:39 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+from mzcoder.config import Config
+
+# Create a Pyrogram client
+app = Client(
+    "my_bot",
+    api_id=Config.API_ID, 
+    api_hash=Config.API_HASH, 
+    bot_token=Config.BOT_TOKEN,
+    plugins=dict(root="mzcoder")
+)
+
+
+
+
+print("I AM ALIVE")
+app.run()
