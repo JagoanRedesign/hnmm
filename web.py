90b5a6744db853d73e37a0b08389f19079f49400
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-10-23 21:27:10 +0700

    Update web.py

--- web.py
+++ web.py
@@ -1,11 +1,37 @@
-from flask import Flask
+import logging
+import asyncio
+from core import bot
+from flask import Flask, jsonify
+import threading
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+async def main():
+    
+    await bot.client.start()
+    
+    logger.info("I AM ALIVE")
+    await bot.idle()  
+    await bot.client.stop()
+    logger.info("bot stopped!")
 
-if __name__ == '__main__':
-    # Run the Flask app 
+@app.route('/')
+def index():
+     return 'Hello, World!'
+
+def run_flask():
     app.run(host='0.0.0.0', port=8000)
+
+if __name__ == "__main__":
+    # Menggunakan event loop default
+    loop = asyncio.get_event_loop()
+    
+    # Menjalankan Flask di thread terpisah
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+    
+    # Menjalankan bot di event loop utama
+    loop.run_until_complete(main())
+    
+    # Tunggu hingga thread Flask selesai sebelum keluar
+    flask_thread.join()
