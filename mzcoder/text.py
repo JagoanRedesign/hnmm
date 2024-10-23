019fc32293d89547b4541a8d490ef6ca83f7dcea
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-10-23 20:56:04 +0700

    Create text.py

---
+++ mzcoder/text.py
@@ -0,0 +1,11 @@
+class Translation(object):
+
+    START_TEXT = '**Hello,** **{0}**  **{1}**\n\n**Send me the link of the video you want to upload**' 
+
+    ABOUT_TXT = """
+╭───────────⍟
+├ **Name** : Project Bot
+├ **Framework** : [PyroFork 2.3.46](https://docs.pyrogram.org/)
+├ **Language** : [Python 3.12.6](https://www.python.org)
+╰───────────────⍟
+"""
