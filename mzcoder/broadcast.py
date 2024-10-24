726376e159b53a63c1a5150e2660c11eacd1c58b
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-10-24 13:38:33 +0700

    Update broadcast.py

--- mzcoder/broadcast.py
+++ mzcoder/broadcast.py
@@ -1,14 +1,13 @@
-import contextlib
 import asyncio
 from pyrogram import Client, filters
-from pyrogram.enums import ChatType
 from pyrogram import errors
+from .user_management import add_user, get_all_users
 
 YOUR_ADMIN_USER_ID = 5166575484  # Ganti dengan ID admin Anda
+add_user(user_id)
 
 @Client.on_message(filters.command("broadcast") & filters.user(YOUR_ADMIN_USER_ID))
 async def broadcast_command(client, message):
-    # Ambil pesan dari perintah
     msg = message.reply_to_message or (message.text.split(maxsplit=1)[1] if len(message.text.split()) > 1 else None)
     
     if not msg:
@@ -16,12 +15,9 @@ async def broadcast_command(client, message):
 
     load = await message.reply("Proses broadcast kepada pengguna...")
     done = error = 0
-    private_chat_ids = []
 
-    # Hanya ambil ID chat dari private chat
-    async for dialog in client.get_dialogs():
-        if dialog.chat.type == ChatType.PRIVATE:  # Hanya ambil chat pribadi
-            private_chat_ids.append(dialog.chat.id)
+    # Mengambil ID pengguna dari database untuk broadcast
+    user_ids = get_all_users()
 
     async def send_broadcast(chat_id):
         nonlocal done, error
@@ -29,18 +25,18 @@ async def broadcast_command(client, message):
             if msg.reply_to_message:
                 await msg.copy(chat_id)
             else:
-                await client.send_message(chat_id, msg.text)  # Menggunakan .text untuk mengirim teks
+                await client.send_message(chat_id, msg.text)
             done += 1
             if done % 4 == 0:
                 await asyncio.sleep(2)
         except errors.FloodWait as f:
-            await asyncio.sleep(f.value + 1)  # Menunggu sesuai waktu yang diberikan
-            await send_broadcast(chat_id)  # Mengulangi pengiriman setelah delay
+            await asyncio.sleep(f.value + 1)
+            await send_broadcast(chat_id)
         except Exception as e:
             print(f"Error sending to {chat_id}: {e}")
             error += 1
 
-    for chat_id in private_chat_ids:
+    for chat_id in user_ids:
         await send_broadcast(chat_id)
 
     if done:
