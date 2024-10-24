1cdb5390a71f8852a0ac47ee436d02c1703e3377
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-10-24 11:53:56 +0700

    Update text.py

--- mzcoder/text.py
+++ mzcoder/text.py
@@ -1,11 +1,22 @@
 class Translation(object):
+    START_TEXT = (
+        "👋 Hello, **{0}** Selamat datang di Bot Download Video Facebook!\n\n"
+        "Untuk menggunakan bot ini, cukup kirimkan tautan video Facebook yang ingin Anda unduh.\n"
+        "Ketika video berhasil diunduh, bot akan mengirimkan video tersebut kepada Anda."
+    )
 
-    START_TEXT = '**Hello,** **{0}**  **{1}**\n\n**Send me the link of the video you want to upload**' 
+    ABOUT_TXT = (
+        "╭───────────⍟\n"
+        "├ **Name** : Project Bot\n"
+        "├ **Framework** : [PyroFork 2.3.46](https://docs.pyrogram.org/)\n"
+        "├ **Language** : [Python 3.12.6](https://www.python.org)\n"
+        "╰───────────────⍟"
+    )
 
-    ABOUT_TXT = """
-╭───────────⍟
-├ **Name** : Project Bot
-├ **Framework** : [PyroFork 2.3.46](https://docs.pyrogram.org/)
-├ **Language** : [Python 3.12.6](https://www.python.org)
-╰───────────────⍟
-"""
+    HELP_TXT = (
+        "📋 Bantuan untuk Menggunakan Bot Download Video Facebook:\n\n"
+        "1. Kirim tautan video Facebook yang valid kepada bot.\n"
+        "2. Bot akan mengunduh video tersebut dan mengirimkannya kembali kepada Anda.\n"
+        "3. Tunggu hingga proses pengunduhan dan pengunggahan selesai.\n\n"
+        "Jika Anda mengalami kesulitan, Anda dapat menghubungi pengembang bot di @{0}."
+    )
