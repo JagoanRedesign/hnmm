7f5ae515205bb093961bbd36e781c34ba0b913f5
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-10-26 09:23:38 +0700

    Update facebook.py

--- mzcoder/facebook.py
+++ mzcoder/facebook.py
@@ -34,6 +34,28 @@ def extract_thumbnail(video_file, thumbnail_file):
         # Ambil frame pada detik ke-1 sebagai thumbnail
         video.save_frame(thumbnail_file, t=1)
 
+async def upload_video_with_progress(client, chat_id, video_file, thumbnail_file, caption):
+    file_size = os.path.getsize(video_file)
+    with open(video_file, 'rb') as video, open(thumbnail_file, 'rb') as thumb:
+        with tqdm(total=file_size, unit='B', unit_scale=True, unit_divisor=1024, desc='Mengunggah') as bar:
+            # Mengunggah video
+            async def upload_chunk():
+                while True:
+                    chunk = video.read(4096)  # Baca dalam potongan 4096 byte
+                    if not chunk:
+                        break
+                    await client.send_video(
+                        chat_id=chat_id,
+                        video=chunk,
+                        thumb=thumb,
+                        caption=caption,
+                        parse_mode=ParseMode.HTML,
+                        supports_streaming=True
+                    )
+                    bar.update(len(chunk))  # Perbarui progress bar
+
+            await upload_chunk()
+
 @Client.on_message(filters.regex(r'https?:\/\/(www\.)?(facebook\.com|fb\.me)\/.*'))
 async def process_facebook_video_link(client, message):
     if Config.CHANNEL:
@@ -84,19 +106,12 @@ async def process_facebook_video_link(client, message):
         await asyncio.sleep(1)
 
         # Upload video to Telegram with thumbnail
-        with open(video_file, 'rb') as video:
-            with open(thumbnail_file, 'rb') as thumb:
-                await client.send_video(
-                    chat_id=message.chat.id,
-                    video=video,
-                    thumb=thumb,
-                    caption=(
-                        f"<b>Size:</b> {os.path.getsize(video_file) / (1024 * 1024):.2f} MB\n"
-                        f"<b>Duration:</b> {duration:.2f} seconds\n"
-                        f"<b>Upload by:</b> @FaceBookDownloadsRobot"
-                    ),
-                    parse_mode=ParseMode.HTML
-                )
+        caption = (
+            f"<b>Size:</b> {os.path.getsize(video_file) / (1024 * 1024):.2f} MB\n"
+            f"<b>Duration:</b> {duration:.2f} seconds\n"
+            f"<b>Upload by:</b> @FaceBookDownloadsRobot"
+        )
+        await upload_video_with_progress(client, message.chat.id, video_file, thumbnail_file, caption)
 
         await uploading_msg.delete()
 
